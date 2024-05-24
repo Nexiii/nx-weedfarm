87cd16f6684de0145e7b42f5d9718896e991e7ed
Malte | NX <58604216+Nexiii@users.noreply.github.com>
2024-05-24 20:24:14 +0200

    Add files via upload

--- Client/plants.lua
+++ Client/plants.lua
@@ -3,6 +3,6 @@ WeedPlants = {
         name = math.random(1, 10000000000),
         coords = vector3(2216.22, 5577.51, 54.56),
         heading = 0,
-        size = vec3(1, 1, 1),
+        size = vec3(1, 1, 2),
     }
 }
--- Server/main.lua
+++ Server/main.lua
@@ -11,6 +11,94 @@ local function checkDistance()
     end
 end
 
+local function LoadESXVersion()
+    ESX = exports["es_extended"]:getSharedObject()
+
+    RegisterNetEvent('nx-weedfarm:GatherCannabis', function()
+        local src = source
+        local xPlayer = ESX.GetPlayerFromId(src)
+        local cannabis = math.random(Config.Picking.ReceiveItem.minAmount, Config.Picking.ReceiveItem.maxAmount)
+        if checkDistance then
+            xPlayer.addInventoryItem('cannabis', cannabis)
+            TriggerClientEvent('esx:showNotification', src, Config.Text["PickedCannabis"], "success")
+        else
+            return false
+        end
+    end)
+
+    RegisterNetEvent('nx-weedfarm:TradingToWeedBaggys', function()
+        local src = source
+        local xPlayer = ESX.GetPlayerFromId(src)
+        local cannabis = xPlayer.getInventoryItem('cannabis')
+        local processingAmount = Config.Processing.pressingConfig.amount
+        local receivingItem = Config.Processing.pressingConfig.receiving
+        if cannabis.count < 1 then
+            TriggerClientEvent('esx:showNotification', src, Config.Text["ErrorProcessingAmount"], "error")
+        end
+        local amount = cannabis.count
+        if amount >= 1 then
+            amount = processingAmount
+        else
+            return false
+        end
+        if cannabis.count >= amount then
+            xPlayer.removeInventoryItem(cannabis, processingAmount)
+            TriggerClientEvent('esx:showNotification', src, Config.Text["CannabisProcessed"] .. processingAmount .. Config.Text["CannabisProcessed1"], "success")
+            Wait(50)
+            xPlayer.addInventoryItem('weed_baggy', receivingItem)
+        else
+            TriggerClientEvent('esx:showNotification', src, Config.Text["ErrorProcessingAmount"], "error")
+            return false
+        end
+    end)
+
+    SellCannabis = {
+        cannabis = math.random(Config.SellPrice.RawCannbis.min, Config.SellPrice.RawCannbis.max)
+    }
+
+    RegisterNetEvent('nx-weedfarm:SellingRawCannabis', function()
+        local src = source
+        local xPlayer = ESX.GetPlayerFromId(src)
+        local price = 0
+        for k, v in pairs(SellCannabis) do
+            local cannabis = xPlayer.getInventoryItem(k)
+            if cannabis and cannabis.count >= 1 then
+                price = price + (v * cannabis.count)
+                xPlayer.removeInventoryItem(k, cannabis.count)
+            end
+        end
+        if price > 0 then
+            xPlayer.addMoney(price)
+            TriggerClientEvent('esx:showNotification', src, Config.Text["successfully_sold"], "success")
+        else
+            TriggerClientEvent('esx:showNotification', src, Config.Text["NoItem"], "error")
+        end
+    end)
+
+    SellBaggys = {
+        weedbaggy = math.random(Config.SellPrice.Baggys.min, Config.SellPrice.Baggys.max)
+    }
+
+    RegisterNetEvent('nx-weedfarm:SellingWeedBaggyss', function()
+        local src = source
+        local xPlayer = ESX.GetPlayerFromId(src)
+        local price = 0
+        for k, v in pairs(SellBaggys) do
+            local weed_baggy = xPlayer.getInventoryItem(k)
+            if weedbaggy and weed_baggy.count >= 1 then
+                price = price + (v * weed_baggy.count)
+                xPlayer.removeInventoryItem(k, weed_baggy.count)
+            end
+        end
+        if price > 0 then
+            xPlayer.addMoney(price)
+            TriggerClientEvent('esx:showNotification', src, Config.Text["successfully_sold1"], "success")
+        else
+            TriggerClientEvent('esx:showNotification', src, Config.Text["NoItem1"], "error")
+        end
+    end)
+end
+
 local function LoadQBVersion()
     local QBCore = exports['qb-core']:GetCoreObject()
     RegisterNetEvent('nx-weedfarm:GatherCannabis', function()
@@ -118,6 +206,8 @@ local function LoadQBVersion()
 
 end
 
-if Config.Framework == "QB" then
+if Config.Framework == "ESX" then
+    LoadESXVersion()
+elseif Config.Framework == "QB" then
     LoadQBVersion()
 end
